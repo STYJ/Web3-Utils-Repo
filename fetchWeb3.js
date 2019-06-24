dc6576a8a645db0bd0737719ec01a1f5ee5156c7
Desmond Ho <howeilun@hotmail.com>
2019-06-24 16:05:05 +0800

    Missing https prefix, stuff works now

--- fetchWeb3.js
+++ fetchWeb3.js
@@ -9,7 +9,7 @@ module.exports = {
   connect: function(network) {
     if (network == 'staging') { network = 'mainnet' }
     const project_id = process.env.INFURA_PROJECT_ID;
-    const infura_url = network + ".infura.io/v3/" + project_id;
+    const infura_url = "https://" + network + ".infura.io/v3/" + project_id;
     const mnemonic = process.env.MNEMONIC;
     const provider = new HDWalletProvider(mnemonic, infura_url, 0, 10);
     const web3 = new Web3(provider);
--- getReserveIndex.js
+++ getReserveIndex.js
@@ -6,7 +6,7 @@ const config_abis = JSON.parse(fs.readFileSync('./config/ABI.json', 'utf8'));
 const config_addresses = JSON.parse(fs.readFileSync('./config/Addresses.json', 'utf8'));
 
 //CHANGE THIS
-NETWORK = "ropsten"
+NETWORK = "staging"
 RESERVES_INDEXES_TO_FETCH = ["0x540f6f1b6920a984ecf1932a6c29036028253835"]
 
 const {addresses, wallets, web3} = connect(NETWORK);
@@ -15,8 +15,7 @@ const kyberNetwork_address = config_addresses[NETWORK].KyberNetwork;
 
 async function main() {
   kyberNetworkContract = new web3.eth.Contract(kyberNetwork_ABI,kyberNetwork_address);
-  reserves = await kyberNetworkContract.methods.feeBurnerContract().call();
-  console.log(reserves)
+  reserves = await kyberNetworkContract.methods.getReserves().call();
   for (i=0;i<reserves.length;i++) {
     for (j=0;j<RESERVES_INDEXES_TO_FETCH.length;j++) {
       reserveAddress = RESERVES_INDEXES_TO_FETCH[j];
@@ -26,6 +25,7 @@ async function main() {
       }
     }
   }
+  process.exit(0);
 }
 
 main()
