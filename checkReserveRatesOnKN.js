68e0371931b8364efef00e4d925af9d6cd0561a3
Desmond Ho <howeilun@hotmail.com>
2019-09-17 11:56:42 +0800

    Requires just reserve address now

--- checkReserveRatesOnKN.js
+++ checkReserveRatesOnKN.js
@@ -8,20 +8,30 @@ const config_params = JSON.parse(fs.readFileSync('./config/liquidity_input_param
 
 //CHANGE THIS
 NETWORK = "staging"
-AUTOMATED_RESERVE_ADDRESS = "0x485c4Ec93D18eBd16623D455567886475aE28D04"
-TOKEN_ADDRESS = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
-TOKEN_DECIMALS = 8
+AUTOMATED_RESERVE_ADDRESS = "0x08030715560a146E306b87CA93Fd618bb2A80363"
 
 const ETH_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 const {addresses, wallets, web3} = connect(NETWORK);
 const kyber_network_proxy_ABI = config_abis.KyberNetworkProxy;
 const kyber_reserve_ABI = config_abis.KyberReserve;
+const liquidity_conversion_rates_ABI = config_abis.LiquidityConversionRates;
 const erc20_token_ABI = config_abis.ERC20;
 const kyber_proxy_address = config_addresses[NETWORK].KyberNetworkProxy;
 
 async function main() {
   kyberProxyInstance = new web3.eth.Contract(kyber_network_proxy_ABI, kyber_proxy_address);
   reserveInstance = new web3.eth.Contract(kyber_reserve_ABI,AUTOMATED_RESERVE_ADDRESS);
+  pricingAddress = await reserveInstance.methods.conversionRatesContract().call((err,res) => {
+    if (err) {
+      stdLog(`Error: Check that reserve address is correct.`,'error');
+      stdLog(`Error: ${err}`);
+    }
+  });
+  pricingInstance = new web3.eth.Contract(liquidity_conversion_rates_ABI, pricingAddress);
+  TOKEN_ADDRESS = await pricingInstance.methods.token().call();
+  tokenInstance = new web3.eth.Contract(erc20_token_ABI, TOKEN_ADDRESS);
+  TOKEN_DECIMALS = await tokenInstance.methods.decimals().call();
+  TOKEN_SYMBOL = await tokenInstance.methods.symbol().call();
 
   const { expectedRate,slippageRate } = await kyberProxyInstance.methods.getExpectedRate(
     ETH_ADDRESS, // srcToken
